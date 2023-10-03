46091cb71fdbffc109520b4efdb27be71a333e53
DAKudryashev <145067538+DAKudryashev@users.noreply.github.com>
2023-10-03 21:21:09 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
-#include <string>
+#include <cstring>
+
+using namespace std;
+
 class FooString{
   char* buf;
 public:
   FooString(char* tbuf){
-    
+    buf=new char[strlen(tbuf) + 1];
+    strcpy(buf, tbuf);
   };
   ~FooString(){
     delete[] buf;
   };
-  void show(a){
-    cout << a;
+  void show(){
+    cout << buf << "\n";
   };
-  int lenght(char* a){
-    int count;
-    for(int i=0; 
+  int lenght(){
+    return strlen(buf);
   };
+};
+
+int main(){
+  FooString a ("testing");
+  int len=a.lenght();
+  cout << len << "\n";
+  a.show();
+  return 0;
 }
